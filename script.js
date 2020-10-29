11dcd116afb05ff3e0479b9d2ed03006df9ab6ff
Smola Karel <ksm@panflex.local>
2020-10-29 20:21:12 +0100

    new Class added

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Class
-class NewWord {
+class NewLine {
     constructor(word, translation, description) {
         this.word = word,
         this.translation = translation,
         this.description = description
     }
+
+    addNewWord(){
+        console.log(this.word)
+    }
+
+    hideTranslation(){
+
+    }
+
+    deleteWord(){
+
+    }
+
 }
+
+// Selectors
+const wordInput = document.getElementById("word-input")
+const translationInput = document.getElementById("translation-input")
+const descriptionInput = document.getElementById("description-input")
+const addBtn = document.querySelector(".add-word-button")
+
+const newWord = new NewLine(wordInput.value, translationInput.value, descriptionInput.value)
+
+addBtn.addEventListener("click", newWord.addNewWord)
